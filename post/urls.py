c9448d4148018174ff2182ac2148748e98e80533
Tran Tien Minh <22520891@gm.uit.edu.vn>
2025-01-03 16:16:23 +0700

    UI and add edit post

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('create-post/', views.create_post, name='create post'),
     path('view-post/<int:post_id>/', views.view_post, name='view post'),
+    path('edit-post/<int:post_id>/', views.edit_post, name='edit post'),
 ]
